e11c1082abdc56bb83d6f8f87c91d50640c98cd0
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-23 12:12:01 +0530

    initial commit

---
+++ src/backend/db/categories.js
@@ -0,0 +1,14 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Category Database can be added here.
+ * You can add category of your wish with different attributes
+ * */
+
+export const categories = [
+  {
+    _id: uuid(),
+    categoryName: "",
+    description: "",
+  },
+];
---
+++ src/backend/db/products.js
@@ -0,0 +1,13 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Product Database can be added here.
+ * You can add products of your wish with different attributes
+ * */
+
+export const products = [
+  {
+    _id: uuid(),
+    categoryName: "",
+  },
+];
