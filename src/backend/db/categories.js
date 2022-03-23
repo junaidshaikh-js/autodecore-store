6e0e180bfbfe72453c50ecad7c1bb65608d731ee
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-24 00:02:52 +0530

    add products and categories in db

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,7 +8,47 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "",
+    categoryName: "Car Mats",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Car Tyres",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Vehicle Cleaners",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Car Lighting",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Car Covers",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Glass Cleaner",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Pressure Washer",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Air Purifiers",
+    description: "",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Vacuum Cleaner",
     description: "",
   },
 ];
