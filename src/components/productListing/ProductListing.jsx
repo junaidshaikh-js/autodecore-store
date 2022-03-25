50a869d430526f93ab93e21a2c071cbba947d456
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-26 01:09:36 +0530

    update the page products with filtered products

--- src/components/productListing/ProductListing.jsx
+++ src/components/productListing/ProductListing.jsx
@@ -3,22 +3,24 @@ import { Filter } from "./Filter";
 import { ProductCard } from "./ProductCard";
 
 import "./product-listing.css";
+import { getFilteredProducts } from "./filters/filter-functions";
 
 export function ProductListing() {
   const {
-    state: { products },
+    state: { products, filters },
   } = useStateContext();
 
+  const filteredProducts = getFilteredProducts(products, filters);
   return (
     <main className="product-listing flex flex-column">
       <>
         <Filter />
 
         <section className="products-section p-1 my-1 mx-1 flex-grow bg-white">
-          <p className="h5">Showing all products ({products.length})</p>
+          <p className="h5">Showing all products ({filteredProducts.length})</p>
 
           <div className="product-container grid mt-2">
-            {products.map((product) => {
+            {filteredProducts.map((product) => {
               return <ProductCard key={product._id} product={product} />;
             })}
           </div>
