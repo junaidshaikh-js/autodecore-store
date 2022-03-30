947e6d116300fa96f7c51cd041ceaa1c56b3c039
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-30 19:13:03 +0530

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,34 @@
+import { FaTwitterSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+
+import "./footer.css";
+
 export function Footer() {
-  return <>Footer</>;
+  const { pathname } = useLocation();
+
+  return (
+    <div
+      className={`bg-black txt-white txt-center footer-wrapper ${
+        pathname == "/products" && "margin-bottom"
+      }`}
+    >
+      <footer className="py-2">
+        <p>Made with ❤️ by Junaid</p>
+        <div className="social-links">
+          <a href="https://twitter.com/junaidshaikh_js" className="mr-sm">
+            <FaTwitterSquare fontSize="1.5rem" />
+            <span className="visually-hidden">Twitter</span>
+          </a>
+          <a href="https://github.com/junaidshaikh-js" className="mr-sm">
+            <FaGithubSquare fontSize="1.5rem" />
+            <span className="visually-hidden">Github</span>
+          </a>
+          <a href="https://www.linkedin.com/in/junaidshaikhjs/">
+            <FaLinkedin fontSize="1.5rem" />
+            <span className="visually-hidden">LinkedIn</span>
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
