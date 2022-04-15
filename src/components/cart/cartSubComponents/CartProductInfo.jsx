f8ef38aaf3badd1851265a5dd59383daf61ff8de
Junaid <sj.shaikhjunaid@gmail.com>
2022-04-15 22:28:40 +0530

    link cart and wishlist card for single product page

--- src/components/cart/cartSubComponents/CartProductInfo.jsx
+++ src/components/cart/cartSubComponents/CartProductInfo.jsx
@@ -1,7 +1,9 @@
 import { ProductHeader, ProductCardPrice } from "../../productListing";
+import { Link } from "react-router-dom";
 
 export function CartProductInfo({
   product: {
+    productId,
     image,
     name,
     soldBy,
@@ -12,20 +14,20 @@ export function CartProductInfo({
   },
 }) {
   return (
-    <div className="card-product-info flex flex-column">
-      <div className="card-image mr-sm  align-self-center">
-        <img className="img-responsive" src={image[0]} alt="product" />
+    <Link to={`/product/${productId}`}>
+      <div className="card-product-info flex flex-column">
+        <div className="card-image mr-sm  align-self-center">
+          <img className="img-responsive" src={image[0]} alt="product" />
+        </div>
+        <div className="card-body ml-sm">
+          <ProductHeader name={name} soldBy={soldBy} rating={rating} />
+          <ProductCardPrice
+            discountedPrice={discountedPrice}
+            originalPrice={originalPrice}
+            discountPercent={discountPercent}
+          />
+        </div>
       </div>
-
-      <div className="card-body ml-sm">
-        <ProductHeader name={name} soldBy={soldBy} rating={rating} />
-
-        <ProductCardPrice
-          discountedPrice={discountedPrice}
-          originalPrice={originalPrice}
-          discountPercent={discountPercent}
-        />
-      </div>
-    </div>
+    </Link>
   );
 }
