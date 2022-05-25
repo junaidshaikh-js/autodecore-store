2fc20ec83c4de038a32240b311a846bf10e729c5
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-25 18:08:47 +0530

    add functionality to address change button

--- src/components/cart/cartSubComponents/ProductDeliverAddress.jsx
+++ src/components/cart/cartSubComponents/ProductDeliverAddress.jsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
+import { useAuth } from "../../../context";
+import { ReactPortal } from "../../ReactPortal/ReactPortal";
+import { SelectAddressModal } from "../../SelectAddressModal/SelectAddressModal";
+
 export function ProductDeliverAddress() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const {
+    state: { addresses },
+  } = useAuth();
+
+  const [currentAddressIndex, setCurrentAddressIndex] = useState(0);
+  const currentAddress = addresses[currentAddressIndex];
+
   return (
     <div className="py-1 border-bottom-dotted flex align-center justify-between">
       <div>
         <span>
           Deliver to:
-          <span className="txt-semibold">Junaid Shaikh</span>
+          <span className="txt-semibold">{` ${currentAddress.name}`}</span>
         </span>
 
         <address className="mt-1">
-          #916 Water St, Ketchikan, Alaska 99901, USA
+          {currentAddress.address}, {currentAddress.city},{" "}
+          {currentAddress.state} - {currentAddress.pincode}
         </address>
       </div>
 
       <div className="ml-2">
-        <button className="btn btn-complementary-outline">Change</button>
+        <button
+          className="btn btn-complementary-outline"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Change
+        </button>
       </div>
+
+      {isModalOpen && (
+        <ReactPortal>
+          <SelectAddressModal
+            currentAddressIndex={currentAddressIndex}
+            setCurrentAddressIndex={setCurrentAddressIndex}
+            setIsModalOpen={setIsModalOpen}
+          />
+        </ReactPortal>
+      )}
     </div>
   );
 }
