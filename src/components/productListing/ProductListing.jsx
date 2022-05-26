d7fc0f8a48e2fa224aae6a2819d57f8daa05cf6f
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-26 16:22:09 +0530

    add image if there is no filtered products

--- src/components/productListing/ProductListing.jsx
+++ src/components/productListing/ProductListing.jsx
@@ -4,6 +4,7 @@ import { ProductCard } from "./ProductCard";
 
 import "./product-listing.css";
 import { getFilteredProducts } from "./filters/filter-functions";
+import NoProductFound from "../../assets/no-result.png";
 
 export function ProductListing() {
   const {
@@ -19,11 +20,21 @@ export function ProductListing() {
         <section className="products-section p-1 my-1 mx-1 flex-grow bg-white">
           <p className="h5">Showing all products ({filteredProducts.length})</p>
 
-          <div className="product-container grid mt-2">
-            {filteredProducts.map((product) => {
-              return <ProductCard key={product._id} product={product} />;
-            })}
-          </div>
+          {!filteredProducts.length ? (
+            <div className="txt-center mt-2">
+              <figure>
+                <img src={NoProductFound} alt="No Product found" />
+              </figure>
+
+              <p className="txt-bold txt-md">No products available</p>
+            </div>
+          ) : (
+            <div className="product-container grid mt-2">
+              {filteredProducts.map((product) => {
+                return <ProductCard key={product._id} product={product} />;
+              })}
+            </div>
+          )}
         </section>
       </>
     </main>
