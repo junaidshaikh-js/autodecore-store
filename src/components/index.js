ce6b2b63e0eacdc9c5532d9196af88c40c8a4ff1
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-26 17:44:45 +0530

    Revert "[fix] netlify deploy"
    This reverts commit f62a0913cadaa2a56f5c22d20fb0fecd6811a34b.

--- src/components/index.js
+++ src/components/index.js
@@ -7,11 +7,15 @@ export * from "./authentication";
 export * from "./footer";
 export * from "./buttons";
 <<<<<<< HEAD
+<<<<<<< HEAD
 export * from "./error-404/Error404";
 =======
 export * from "./Error-404";
 export * from "./Toast/Toast";
 >>>>>>> parent of f2d1e68 ([fix] file and folder names)
+=======
+export * from "./error-404";
+>>>>>>> parent of f62a091 ([fix] netlify deploy)
 export * from "./singleProductPage/SingleProductPage";
 export * from "./userProfile";
 export * from "./OrderSummary/OrderSummary";
